c59c51d2268d919204afed66a939a379acc4e33d
J-Guenther <jannik.guentheg@gmail.com>
2022-11-12 13:55:50 +0100

    added todo

--- src/index.ts
+++ src/index.ts
@@ -7,3 +7,4 @@ export {parse} from './parse'
 // TODO parser needs to handle parenthesis (subgroups)
 // TODO parser and tokenizer needs to handle list of columns instead of only "*"
 // TODO add Runner
+// TODO make Layer an array like datastructur, so Runner can use .filter(expression) on it
